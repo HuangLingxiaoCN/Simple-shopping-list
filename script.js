f453ea6b78ecdddb12f801a74b8e399a73413308
JasonHuang1213 <37361235+JasonHuang1213@users.noreply.github.com>
2020-08-23 22:08:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,60 @@
-// ------------------ Select elements ---------------------
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var list = document.querySelector("ul");
+// querySelector('selector') - matches a specified CSS selector
 
-// ------------------ functions ---------------------------
+// 1. toggle .done class ----------------------------
+var item = document.getElementsByTagName("li");
+
+function toggleListElement() {
+	for(var i = 0; i < item.length; i++){
+		item[i].addEventListener("click", toggle)
+	}
+}
+function toggle() {
+	this.classList.toggle("done");
+	// this refers to the current object
+	// classList.toggle() / classList.add()
+}
+//---------------------------------------------------
+
+// 2. Add delete buttons and functions---------------
+// Needs improvement. Cannot delete specific li
+var btn = document.querySelectorAll("li button");
+
+for(i = 0; i< btn.length; i++){
+	btn[i].addEventListener("click", () => {
+		list.removeChild(item[0]);
+	});
+}
+
+
+//---------------------------------------------------
+
 function inputLength() {
-  return input.value.length;
+	return input.value.length;
 }
 
 function createListElement() {
-  var li = document.createElement("li");
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = ""; // reset the placeholder to empty
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	list.appendChild(li);
+	li.addEventListener("click",toggle);
+	input.value = "";
 }
 
 function addListAfterClick() {
-  if(inputLength() > 0){ // input.value != ""
-    createListElement();
-  }
+	if (inputLength() > 0) {
+		createListElement();
+	}
 }
 
-function addListAfterKeyPress(event) {
-  if(inputLength() > 0 && event.keyCode === 13){ // input.value != ""
-    createListElement();
-  }
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
 }
 
-// ------------------ button and input events -------------
-// addListAfterClick and addListAfterKeyPress are called
-// Callback functions (not run until event happens)
 button.addEventListener("click", addListAfterClick);
-input.addEventListener("keypress", addListAfterKeyPress);
+input.addEventListener("keypress", addListAfterKeypress);
+toggleListElement(); // 1. toggle done class
